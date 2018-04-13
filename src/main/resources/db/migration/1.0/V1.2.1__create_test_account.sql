fbb35e416702cbc7aa212ec24293d12c66846cac
Aaron Nwabuoku <aerisvulpe@gmail.com>
2018-04-12 21:03:26 -0400

    Change test password and persist database for lifespan of the JVM.

--- src/main/resources/db/migration/1.0/V1.2.1__create_test_account.sql
+++ src/main/resources/db/migration/1.0/V1.2.1__create_test_account.sql
@@ -1,2 +1,2 @@
 INSERT INTO account (id, email, first_name, last_name, password, username)
-VALUES (1, 'johndoe@app.com', 'John', 'Doe', '$2a$12$41mZHppp1HE1/1G47ECKkuLlccCfXVzNzsob6cxOmuizC54CIMX9K', 'admin');
+VALUES (1, 'johndoe@app.com', 'John', 'Doe', '$2a$04$AkU4eG.W9Iiz8HcKTYhXp.OerF5tuzBHi8nd4OFXDol.oOHXc1lYu', 'admin');
